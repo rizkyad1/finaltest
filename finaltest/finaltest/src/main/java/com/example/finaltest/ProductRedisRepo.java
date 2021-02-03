b7c2cde63dd6d32ac43ab53acde1f4d102a3719c
rizkyad1 <38678717+rizkyad1@users.noreply.github.com>
2021-02-03 13:25:54 +0800

    Delete finaltest directory

--- finaltest/finaltest/src/main/java/com/example/finaltest/ProductRedisRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.finaltest;
-
-import com.example.finaltest.model.ProductRedis;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRedisRepo extends CrudRepository<ProductRedis, String> {
-}
--- finaltest/src/main/java/com/example/finaltest/jwt/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.finaltest.jwt;
-
-import com.example.finaltest.jwt.model.UserDao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserDao, Integer> {
-    UserDao findByUsername(String name);
-}
